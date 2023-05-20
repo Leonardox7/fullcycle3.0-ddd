372bd94135f2a74f42fd18786a58022041be3748
Leonardox7 <leonardoz5z@hotmail.com>
2023-05-20 17:42:12 -0300

    feat: Adiciona eventos para notificar quando cliente e registrado e endereco alterado

--- src/domain/@shared/event/event-dispatcher.spec.ts
+++ src/domain/@shared/event/event-dispatcher.spec.ts
@@ -1,3 +1,10 @@
+import Customer from "../../customer/entity/customer";
+import AddressChangedEvent from "../../customer/event/address-changed.event";
+import CustomerCreatedEvent from "../../customer/event/customer-cratead.event";
+import SendConsoleLogHandler from "../../customer/event/send-log-when-address-is-changed.handler";
+import SendConsoleLog1Handler from "../../customer/event/send-log1-when-customer-is-created.handler";
+import SendConsoleLog2Handler from "../../customer/event/send-log2-when-customer-is-created.handler";
+import Address from "../../customer/value-object/address";
 import SendEmailWhenProductIsCreatedHandler from "../../product/event/handler/send-email-when-product-is-created.handler";
 import ProductCreatedEvent from "../../product/event/product-created.event";
 import EventDispatcher from "./event-dispatcher";
@@ -79,4 +86,96 @@ describe("Domain events tests", () => {
 
     expect(spyEventHandler).toHaveBeenCalled();
   });
+
+  it("should register an event customer created and use SendConsoleLog1 handler", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendConsoleLog1Handler();
+
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"]
+    ).toBeDefined();
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"].length).toBe(
+      1
+    );
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]
+    ).toMatchObject(eventHandler);
+  });
+
+  it("should notify SendConsoleLog1 event handlers", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendConsoleLog1Handler();
+    const spyEventHandler = jest.spyOn(eventHandler, "handle");
+
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]
+    ).toMatchObject(eventHandler);
+
+    const customerCreatedEvent = new CustomerCreatedEvent(new Customer("1", "Test"));
+
+    eventDispatcher.notify(customerCreatedEvent);
+
+    expect(spyEventHandler).toHaveBeenCalled();
+  });
+
+
+  it("should register an event customer created and use SendConsoleLog2 handler", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendConsoleLog2Handler();
+
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"]
+    ).toBeDefined();
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"].length).toBe(
+      1
+    );
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]
+    ).toMatchObject(eventHandler);
+  });
+
+  it("should notify SendConsoleLog2 event handlers when customer is created", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendConsoleLog2Handler();
+    const spyEventHandler = jest.spyOn(eventHandler, "handle");
+
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]
+    ).toMatchObject(eventHandler);
+
+    const customerCreatedEvent = new CustomerCreatedEvent(new Customer("1", "Test"));
+
+    eventDispatcher.notify(customerCreatedEvent);
+
+    expect(spyEventHandler).toHaveBeenCalled();
+  });
+
+  it("should notify SendConsoleLog event handlers when address is changed", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendConsoleLogHandler();
+    const spyEventHandler = jest.spyOn(eventHandler, "handle");
+
+    eventDispatcher.register("AddressChangedEvent", eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers["AddressChangedEvent"][0]
+    ).toMatchObject(eventHandler);
+
+    const customer = new Customer("1", "Test");
+    customer.changeAddress(new Address("street 1", 2, "302010", "BH"))
+    const addressChangedEvent = new AddressChangedEvent(customer);
+
+    eventDispatcher.notify(addressChangedEvent);
+
+    expect(spyEventHandler).toHaveBeenCalled();
+  });
+
 });
